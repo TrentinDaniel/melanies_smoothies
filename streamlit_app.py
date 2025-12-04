54fb17a78b9ded48e613668a3683ea3fb821f871
TrentinDaniel <131895121+TrentinDaniel@users.noreply.github.com>
2025-12-04 14:08:52 -0500

    Remove duplicate requests for watermelon API

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,3 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
